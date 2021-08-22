6ea930a573926e7c358e16f30429a6f865922df0
Captain Chicky <maxfang2015@yahoo.com>
2021-08-22 19:08:07 -0400

    uhhhh

---
+++ Archive/webstatic-sea.mihoyo.com/ys/event/im-service/3_5b90ded36b5a9753078a.js
@@ -0,0 +1,2 @@
+/*! Copyright © 2011 - 2020 miHoYo. All Rights Reserved */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{683:function(n,w,o){}}]);
---
+++ Archive/webstatic-sea.mihoyo.com/ys/event/im-service/3_cb81e6a804bd218e59ce.js
@@ -0,0 +1,2 @@
+/*! Copyright © 2011 - 2021 miHoYo. All Rights Reserved */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{685:function(n,w,o){}}]);
