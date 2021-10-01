bcb5222dd163a1fe770384bc6d8a6f47aed82179
Captain Chicky <maxfang2015@yahoo.com>
2021-10-01 19:38:33 -0400

    e

--- Archive/TestingStuff/script.js
+++ Archive/TestingStuff/script.js
@@ -3,8 +3,19 @@ var portfolio = [
         category: 'Example',
         projects: [
             {
-                title: "Description",
-                description: "text",
+                title: "Title1",
+                description: "text1",
+                image: "arcwallpaper.png",
+                link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            }
+        ]
+    },
+    {
+        category: 'Example2',
+        projects: [
+            {
+                title: "Title2",
+                description: "text2",
                 image: "arcwallpaper.png",
                 link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
             }
