3ded627e692d7da5dc772d38e8dac750345e1129
Captain Chicky <maxfang2015@yahoo.com>
2022-01-24 23:20:38 -0500

    Update thing.js

--- School/Bio/thing.js
+++ School/Bio/thing.js
@@ -23,95 +23,95 @@ function convert(rawDNA){
   //part 1
   for (var i = 0; i < rawDNA.length; i++) {
   var temp2 = rawDNA[i].toUpperCase();
-  DNAinput.append(temp2);
+  DNAinput.push(temp2);
   }
   //part 2
   for (var i = 0; i < DNAinput.length; i++) {
     if (DNAinput[i] == "T") {
-      mRNAoutput.append("A");
+      mRNAoutput.push("A");
     }
     else if (DNAinput[i] == "A") {
-      mRNAoutput.append("U");
+      mRNAoutput.push("U");
     } 
     else if (DNAinput[i] == "C") {
-      mRNAoutput.append("G");
+      mRNAoutput.push("G");
     }
     else if (DNAinput[i] == "G") {
-      mRNAoutput.append("C");
+      mRNAoutput.push("C");
     }
   }
   //part 3
   for (var i = 0; i < mRNAoutput.length-2; i+=3) {
     var temp = mRNAoutput[i] + mRNAoutput[i+1] + mRNAoutput[i+2];
-    mRNAinput.append(temp);
+    mRNAinput.push(temp);
   }
   //paert 4
   for (var i = 0; i < mRNAinput.length; i++) {
     if (mRNAinput[i] == "UUU" || mRNAinput[i] == "UUC") {
-      AAoutput.append("Phe");
+      AAoutput.push("Phe");
     }
     else if (mRNAinput[i] == "UUA" || mRNAinput[i] == "UUG" || mRNAinput[i] == "CUU" || mRNAinput[i] == "CUC" || mRNAinput[i] == "CUA" || mRNAinput[i] == "CUG") {
-      AAoutput.append("Leu");
+      AAoutput.push("Leu");
     } 
     else if (mRNAinput[i] == "AUU" || mRNAinput[i] == "AUC" || mRNAinput[i] == "AUA") {
-      AAoutput.append("Ile");
+      AAoutput.push("Ile");
     }
     else if (mRNAinput[i] == "AUG") {
-      AAoutput.append("Met");
+      AAoutput.push("Met");
     }
     else if (mRNAinput[i] == "GUU" || mRNAinput[i] == "GUC" || mRNAinput[i] == "GUA" || mRNAinput[i] == "GUG") {
-      AAoutput.append("Val");
+      AAoutput.push("Val");
     }
     else if (mRNAinput[i] == "UCU" || mRNAinput[i] == "UCC" || mRNAinput[i] == "UCA" || mRNAinput[i] == "UCG" || mRNAinput[i] == "AGU" || mRNAinput[i] == "AGC") {
-      AAoutput.append("Ser");
+      AAoutput.push("Ser");
     }
     else if (mRNAinput[i] == "CCU" || mRNAinput[i] == "CCC" || mRNAinput[i] == "CCA" || mRNAinput[i] == "CCG") {
-      AAoutput.append("Pro");
+      AAoutput.push("Pro");
     }
     else if (mRNAinput[i] == "ACU" || mRNAinput[i] == "ACC" || mRNAinput[i] == "ACA" || mRNAinput[i] == "ACG") {
-      AAoutput.append("Thr");
+      AAoutput.push("Thr");
     }
     else if (mRNAinput[i] == "GCU" || mRNAinput[i] == "GCC" || mRNAinput[i] == "GCA" || mRNAinput[i] == "GCG") {
-      AAoutput.append("Ala");
+      AAoutput.push("Ala");
     }
     else if (mRNAinput[i] == "UAU" || mRNAinput[i] == "UAC") {
-      AAoutput.append("Tyr");
+      AAoutput.push("Tyr");
     }
     else if (mRNAinput[i] == "CAU" || mRNAinput[i] == "CAC") {
-      AAoutput.append("His");
+      AAoutput.push("His");
     }
     else if (mRNAinput[i] == "CAA" || mRNAinput[i] == "CAG") {
-      AAoutput.append("Gln"); 
+      AAoutput.push("Gln"); 
     }
     else if (mRNAinput[i] == "AAU" || mRNAinput[i] == "AAC") {
-      AAoutput.append("Asn"); 
+      AAoutput.push("Asn"); 
     }
     else if (mRNAinput[i] == "AAA" || mRNAinput[i] == "AAG") {
-      AAoutput.append("Lys");
+      AAoutput.push("Lys");
     }
     else if (mRNAinput[i] == "GAU" || mRNAinput[i] == "GAC") {
-      AAoutput.append("Asp");
+      AAoutput.push("Asp");
     }
     else if (mRNAinput[i] == "GAA" || mRNAinput[i] == "GAG") {
-      AAoutput.append("Glu");
+      AAoutput.push("Glu");
     }
     else if (mRNAinput[i] == "UGU" || mRNAinput[i] == "UGC") {
-      AAoutput.append("Cys"); 
+      AAoutput.push("Cys"); 
     }
     else if (mRNAinput[i] == "UGG") {
-      AAoutput.append("Trp");
+      AAoutput.push("Trp");
     }
     else if (mRNAinput[i] == "CGY" || mRNAinput[i] == "CGC" || mRNAinput[i] == "CGA" || mRNAinput[i] == "CGG") {
-      AAoutput.append("Arp"); 
+      AAoutput.push("Arp"); 
     }
     else if (mRNAinput[i] == "AGA" || mRNAinput[i] == "AGG") {
-      AAoutput.append("Arg");
+      AAoutput.push("Arg");
     }
     else if (mRNAinput[i] == "GGU" || mRNAinput[i] == "GGC" || mRNAinput[i] == "GGA" || mRNAinput[i] == "GGG") {
-      AAoutput.append("Gly");
+      AAoutput.push("Gly");
     }
     else if (mRNAinput[i] == "UAA" || mRNAinput[i] == "UAG" || mRNAinput[i] == "UGa") {
-      AAoutput.append("Stop");
+      AAoutput.push("Stop");
     }
   }
   return AAoutput;
